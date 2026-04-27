ff6b4b61383848fb30c19da85193a13732a84a87
Diksha karna <dikshakarna071@gmail.com>
2026-04-27 10:48:38 +0545

    remove from cart

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -15,3 +15,17 @@ export const CartProvider = ({ children }) => {
       return [...prevCart, { ...product, quantity: 1 }];
     });
   };
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  const cartTotal = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, cartTotal }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
