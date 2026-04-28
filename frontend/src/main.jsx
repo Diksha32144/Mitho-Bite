e0be10feadd1659cd50e3e4d7745019f4801f18c
Diksha karna <dikshakarna071@gmail.com>
2026-04-28 08:50:14 +0545

    cart page started and remove cartsidebar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-// Import the CartProvider you just created
+
 import { CartProvider } from './context/CartContext' 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* Wrap App with the Provider to share cart data everywhere */}
-    <CartProvider>
-      <App />
-    </CartProvider>
+ <StrictMode>
+    <BrowserRouter> 
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ frontend/src/pages/CartPage.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { useCart } from '../context/CartContext';
+import { Trash2, Plus, Minus, ArrowLeft } from 'lucide-react';
+import { Link } from 'react-router-dom';
