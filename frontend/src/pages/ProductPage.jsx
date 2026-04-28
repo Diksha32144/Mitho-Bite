c503b1c1cc87f27eb68cd3e11d299ddfdb121af8
Diksha karna <dikshakarna071@gmail.com>
2026-04-28 09:34:37 +0545

    function and variabled dec;ered

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,3 +1,9 @@
 import React, {useState} from 'react';
 import { useCart } from '../context/CartContext';
 import { Search, Star, ShoppingCart } from 'lucide-react';
+
+export default function ProductPage({ products }) {
+  const { addToCart } = useCart();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('All Products');
+}
