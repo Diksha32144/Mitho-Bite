9248034eb6dbc193e0a49c99e6b3c456ab438beb
Diksha karna <dikshakarna071@gmail.com>
2026-04-29 09:48:55 +0545

    checkoutpage started

---
+++ frontend/src/pages/CheckoutPage.jsx
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { useCart } from '../context/CartContext';
+import axios from 'axios';
+import { User, MapPin, CreditCard, ChevronLeft } from 'lucide-react';
