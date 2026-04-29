d434af9a19b3fb2ad4c9ccee6d7f9e711e9038e9
Diksha karna <dikshakarna071@gmail.com>
2026-04-29 09:50:00 +0545

    checkout item listed

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -2,3 +2,16 @@ import React, { useState } from 'react';
 import { useCart } from '../context/CartContext';
 import axios from 'axios';
 import { User, MapPin, CreditCard, ChevronLeft } from 'lucide-react';
+
+const CheckoutPage = () => {
+  const { cart, totalPrice, clearCart } = useCart();
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    street: '',
+    city: '',
+    zip: '',
+    paymentMethod: 'Cash on Delivery'
+  });
