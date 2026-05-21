f56fa9298b8bb0426858e9b5132524f15e5e54a2
Diksha karna <dikshakarna071@gmail.com>
2026-05-21 20:08:24 +0545

    handle login added

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,5 +3,23 @@ import {Link, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
 export default function LoginPage(){
-    
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+
+const handleLogin = async (e) =>{
+    e.preventDefault();
+    setError('');
+    try
+}
+
+
+
+
+
+
+
+
+
 }
